2132edbeb951b3800d2368c33ff687ad9bafa7df
Zephyr Pellerin <zv@nxvr.org>
2017-10-29 13:28:36 -0700

    update phrases for speech

--- frontend/src/pages/home/home.ts
+++ frontend/src/pages/home/home.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    brightness: number = 20;
+    contrast: number = 0;
+    warmth: number = 1300;
+    structure: any = { lower: 33, upper: 60 };
+    text: number = 0;
 
-  }
+    constructor(public navCtrl: NavController) {
+
+    }
 
 }
